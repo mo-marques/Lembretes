6ff6c67db091ea1b79151d23bd2b1f079ba6cd20
Mônica Marques <mo.menezes.marques@gmail.com>
2021-08-16 20:58:47 -0300

    projeto funcionando, necessario alguns ajustes visuais

--- app/src/main/java/com/example/lembretes/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/lembretes/ui/AddTaskActivity.kt
@@ -1,10 +1,14 @@
 package com.example.lembretes.ui
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lembretes.databinding.ActivityAddTaskBinding
+import com.example.lembretes.databinding.ActivityMainBinding
+import com.example.lembretes.datasource.TaskDataSource
 import com.example.lembretes.extensions.format
 import com.example.lembretes.extensions.text
+import com.example.lembretes.model.Task
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
@@ -18,6 +22,17 @@ class AddTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?. let {
+                binding.tilTitle.text = it.title
+                binding.tilDescricao.text = it.description
+                binding.tilData.text = it.date
+                binding.tilHora.text = it.hour
+
+            }
+        }
 
         insertListeners()
 
@@ -41,12 +56,37 @@ class AddTaskActivity : AppCompatActivity() {
                 .setTimeFormat(TimeFormat.CLOCK_24H)
                 .build()
             timePicker.addOnPositiveButtonClickListener {
-                binding.tilHora.text = "${timePicker.hour} ${timePicker.minute}"
+                val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+                binding.tilHora.text = "$hour:$minute"
 
             }
 
             timePicker.show(supportFragmentManager, null)
 
         }
+
+        binding.btnCancel.setOnClickListener {
+            finish()
+        }
+        binding.btnCriar.setOnClickListener {
+        val task = Task(
+            title = binding.tilTitle.text,
+            description = binding.tilDescricao.text,
+            date = binding.tilData.text,
+            hour = binding.tilHora.text,
+            id = intent.getIntExtra(TASK_ID, 0)
+
+        )
+            TaskDataSource.insertTask(task)
+
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+        
+    }
+
+    companion object {
+        const val TASK_ID = "task_id"
     }
 }
